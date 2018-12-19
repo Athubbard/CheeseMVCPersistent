641b3c0585b2c1d00c2e9f88e5ed6bda94ae77d8
Amber Hubbard <amberthubbard@gmail.com>
2018-12-18 21:00:49 -0600

    Initial commit

--- src/CheeseMVC/Migrations/20181218001141_InitialMigration.cs
+++ src/CheeseMVC/Migrations/20181218001141_InitialMigration.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
-using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace CheeseMVC.Migrations
 {
@@ -10,25 +8,23 @@ namespace CheeseMVC.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-                name: "Cheeses",
+                name: "Categories",
                 columns: table => new
                 {
                     ID = table.Column<int>(nullable: false)
                         .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    Description = table.Column<string>(nullable: true),
-                    Name = table.Column<string>(nullable: true),
-                    Type = table.Column<int>(nullable: false)
+                    Name = table.Column<string>(nullable: true)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Cheeses", x => x.ID);
+                    table.PrimaryKey("PK_Categories", x => x.ID);
                 });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "Cheeses");
+                name: "Categories");
         }
     }
 }
